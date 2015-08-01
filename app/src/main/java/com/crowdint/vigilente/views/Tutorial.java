184617b213ee2892116cda13907e698eaddd0336
DarkAbsolutE <dark_necros_absolute@hotmail.com>
2015-08-01 18:21:16 -0500

    Adding flow of Tutorial

--- app/src/main/java/com/crowdint/vigilente/views/Tutorial.java
+++ app/src/main/java/com/crowdint/vigilente/views/Tutorial.java
@@ -1,10 +1,14 @@
 package com.crowdint.vigilente.views;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 
+import com.crowdint.vigilente.ViewCamera;
 import com.crowdint.vigilente.R;
+import com.crowdint.vigilente.utilities.PreferencesManager;
 
 /**
  * Created by DarkAbsolutE on 7/29/15.
@@ -18,4 +22,43 @@ public class Tutorial extends Activity {
         setContentView(R.layout.tutorial);
     }
 
+    public void nextView(View view) {
+        check();
+    }
+
+    public void check() {
+        if (checkNextView()) {
+            saveOption();
+            changeViewCamera();
+        } else {
+            changeViewTandC();
+        }
+    }
+
+    public void saveOption() {
+        PreferencesManager.SavePreferences("tutorial", "1", this);
+    }
+
+    public void changeViewTandC() {
+        Intent intent = new Intent(this, TandC.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void changeViewCamera() {
+        Intent intent = new Intent(this, ViewCamera.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public boolean checkNextView() {
+        //Check for change to view tutorial and not show the view t&c
+        if (PreferencesManager.ReadPreferences("t&c", this).equals("1")) {
+            return true;
+        }
+        //Check for change to view tutorial and show the view t&c
+        else {
+            return false;
+        }
+    }
 }
