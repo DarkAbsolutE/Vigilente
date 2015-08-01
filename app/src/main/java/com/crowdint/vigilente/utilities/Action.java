cc51a7e3d1cba61579ba8334fa85e918dfd99258
DarkAbsolutE <dark_necros_absolute@hotmail.com>
2015-08-01 18:38:11 -0500

    Create class for variables of the gallery

---
+++ app/src/main/java/com/crowdint/vigilente/utilities/Action.java
@@ -0,0 +1,9 @@
+package com.crowdint.vigilente.utilities;
+
+/**
+ * Created by DarkAbsolutE on 8/1/15.
+ */
+public class Action {
+    public static final String ACTION_PICK = "luminous.ACTION_PICK";
+    public static final String ACTION_MULTIPLE_PICK = "luminous.ACTION_MULTIPLE_PICK";
+}
